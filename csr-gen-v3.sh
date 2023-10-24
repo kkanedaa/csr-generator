212b4418b81774a05962cdfe1e4e501f641cd884
Kei Kaneda <kkanedaa@gmail.com>
2023-10-24 08:25:34 +0200

    Added validation for custom option

--- csr-gen-v3.sh
+++ csr-gen-v3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #-----------------------------------#
-# Configure the values below        # 
+#    Configure the values below     # 
 #-----------------------------------#
 
 # Config for Email DV
@@ -20,7 +20,7 @@ tls_san2="www.kaneda.ch"
 
 function generate_config() {
 
-  cat <<EOF > tmp.conf
+  cat <<EOF > ${conf_file}
     [req]
     distinguished_name = req_distinguished_name
     req_extensions = v3_req
@@ -44,27 +44,22 @@ EOF
 
   case ${TYPE} in
     "email-dv")
-      sed -i "s/CN =/CN = $dv_email/g" tmp.conf
-      sed -i "s/emailAddress =/emailAddress = $dv_email/g" tmp.conf
-      sed "3d;13,19d" tmp.conf > tmp_edit.conf
-      rm tmp.conf
+      sed -i "s/CN =/CN = $dv_email/g" ${conf_file}
+      sed -i "s/emailAddress =/emailAddress = $dv_email/g" ${conf_file}
+      sed -i -e "3d;13,19d" ${conf_file}
     ;;
     "email-ov")
-      sed -i "s/CN =/CN = $ov_cn/g" tmp.conf
-      sed -i "s/emailAddress =/emailAddress = $ov_email/g" tmp.conf
-      sed "3d;13,19d" tmp.conf > tmp_edit.conf
-      rm tmp.conf
+      sed -i "s/CN =/CN = $ov_cn/g" ${conf_file}
+      sed -i "s/emailAddress =/emailAddress = $ov_email/g" ${conf_file}
+      sed -i -e "3d;13,19d" ${conf_file}
     ;;
     "tls")
-      sed -i "s/CN =/CN = $tls_cn/g" tmp.conf
-      sed -i "s/DNS.1 =/DNS.1 = $tls_san1/g" tmp.conf
-      sed -i "s/DNS.2 =/DNS.2 = $tls_san2/g" tmp.conf
-      sed "12d" tmp.conf > tmp_edit.conf
-      rm tmp.conf
+      sed -i "s/CN =/CN = $tls_cn/g" ${conf_file}
+      sed -i "s/DNS.1 =/DNS.1 = $tls_san1/g" ${conf_file}
+      sed -i "s/DNS.2 =/DNS.2 = $tls_san2/g" ${conf_file}
+      sed -i -e "12d" ${conf_file}
     ;;
-    "custom") 
-      mv tmp.conf tmp_edit.conf
-    ;;
+    "custom") : ;;
   esac
 
   # Remove leading whitespaces
@@ -73,13 +68,11 @@ EOF
 
 function current_config() {
 
-  conf_file=tmp_edit.conf
-
   # Get values in the config file
-  conf_cn=$(grep -i cn ${conf_file})
-  conf_san1=$(grep -i DNS.1 ${conf_file})
-  conf_san2=$(grep -i DNS.2 ${conf_file})
-  conf_email=$(grep -i email ${conf_file})
+  conf_cn=$(grep -i "cn" ${conf_file})
+  conf_san1=$(grep -i "DNS.1" ${conf_file})
+  conf_san2=$(grep -i "DNS.2" ${conf_file})
+  conf_email=$(grep -i "email" ${conf_file})
 
   # check if var is not empty and print value
   [ -n "$conf_cn" ] && echo -e "${conf_cn}"
@@ -91,7 +84,7 @@ function current_config() {
 
 function generate_csr() {
   
-  openssl req -newkey rsa:2048 -keyout ${filename}.key -out ${filename}.csr -config tmp_edit.conf -nodes
+  openssl req -newkey rsa:2048 -keyout ${filename}.key -out ${filename}.csr -config ${conf_file} -nodes
   echo -e "\nCSR for ${TYPE} certificate:\n"
 
   current_config
@@ -102,23 +95,21 @@ function generate_csr() {
 }
 
 function edit_conf() {
-
-  conf_file=tmp_edit.conf  
-
   echo -e ""
   read -p "CN: " set_cn
   read -p "SAN 1: " set_san1
   read -p "SAN 2: " set_san2
   read -p "Email: " set_email
 
-  sed -i "s/CN =/CN = $set_cn/g" $conf_file
-  sed -i "s/DNS.1 =/DNS.1 = $set_san1/g" $conf_file
-  sed -i "s/DNS.2 =/DNS.2 = $set_san2/g" $conf_file
-  sed -i "s/emailAddress =/emailAddress = $set_email/g" $conf_file
+  [ -n "$set_cn" ] && sed -i "s/CN =/CN = ${set_cn}/g" ${conf_file} || echo -e "CN cannot be empty."
+  # If SAN1 (DNS.1) is not set, the vlaue will be the one specified in CN
+  [ -n "$set_san1" ] && sed -i "s/DNS.1 =/DNS.1 = ${set_san1}/g" ${conf_file} || sed -i "s/DNS.1 =/DNS.1 = ${set_cn}/g" ${conf_file}
+  [ -n "$set_san2" ] && sed -i "s/DNS.2 =/DNS.2 = ${set_san2}/g" ${conf_file} || sed -i -e "/DNS.2 =/d" ${conf_file}
+  [ -n "$set_email" ] && sed -i "s/emailAddress =/emailAddress = ${set_email}/g" ${conf_file} || sed -i -e "/emailAddress =/d" ${conf_file}
 
-  generate_csr
 }
 
+conf_file=tmp.conf
 filename=sample
 
 while true
@@ -133,7 +124,7 @@ do
     "1") TYPE="email-dv"; generate_config; generate_csr ${TYPE} ;;
     "2") TYPE="email-ov"; generate_config; generate_csr ${TYPE} ;;
     "3") TYPE="tls"; generate_config; generate_csr ${TYPE} ;;
-    "4") TYPE="custom"; generate_config; edit_conf ;;
+    "4") TYPE="custom"; generate_config; edit_conf; generate_csr ${TYPE} ;;
     *) continue ;;
   esac
 
